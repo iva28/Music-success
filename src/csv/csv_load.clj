1419a3b08c2e472662d7d7a530594e8dce15acab
iva28 <iva.hurt574@gmail.com>
2024-05-16 00:08:56 +0200

    Name of columns

--- src/csv/csv_load.clj
+++ src/csv/csv_load.clj
@@ -11,3 +11,19 @@
 (def stones-csv (read-csv "src/dataset/stones_analysis.csv"))
 
 (take 1 stones-csv)
+
+(print (count stones-csv))
+
+
+;Column names
+(def csv-column-names (vec (first stones-csv)))
+(print csv-column-names)
+(count csv-column-names)
+
+(defn index-of-element-by-name
+  [seq name]
+   (.indexOf seq name))
+
+(def first-award-column (index-of-element-by-name csv-column-names "British charts"))
+(print first-award-column)
+
