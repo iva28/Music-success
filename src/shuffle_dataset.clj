0141ad2ff97c0b317dd1f2a5f9dd929fc89f3562
iva28 <iva.hurt574@gmail.com>
2024-05-16 12:02:12 +0200

    Shuffled songs so that they are not sorted by album name

--- src/shuffle_dataset.clj
+++ src/shuffle_dataset.clj
@@ -1,5 +1,22 @@
 (ns shuffle_dataset
-  (:require [csv_load :refer [read-csv]]))
+  (:require [csv_load :refer [read-csv save-modified-songs-csv]]
+            [shuffle_functions :refer[add-random-element-from-list-of-list]]))
 
 (def stones-csv (read-csv "src/dataset/modified_stones.csv"))
 (count stones-csv)
+
+;Shuffling origininal csv
+(def column-names (first stones-csv))
+(print column-names)
+(def stones-songs (rest stones-csv))
+
+(def  shuffled-stones (add-random-element-from-list-of-list stones-songs))
+
+(first stones-songs)
+(first shuffled-stones)
+
+;Save shuffled
+(save-modified-songs-csv shuffled-stones column-names "src/dataset/shuffled_songs.csv")
+
+
+
