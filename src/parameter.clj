ee43cde32bf5efe6a8a9799f4cd9eb842ffaca31
iva28 <iva.hurt574@gmail.com>
2024-05-16 15:22:12 +0200

    Distance between two songs

--- src/parameter.clj
+++ src/parameter.clj
@@ -1,12 +1,19 @@
 (ns parameter
   (:require [csv_load :refer [read-csv]]
-            [conversion :refer [convert-to-float-list-of-lists]]
-            [distance_functions :refer [calculate-euclidean-distance]]
+            [conversion :refer [convert-to-float-in-list
+                                convert-to-float-list-of-lists]]
+            [distance_functions :refer [calculate-euclidean-distance-csv]]
             [utility_functions :refer [find-by-name]]))
 
 (def stones-csv (read-csv "src/dataset/modified_stones.csv"))
 (def stones-songs (rest stones-csv))
 
-(defn calculate-3-closest-songs
-  [song songs]
-  ())
+(def first-song (first stones-songs))
+(def second-song (second stones-songs))
+
+(def first-song (convert-to-float-in-list first-song))
+(def second-song (convert-to-float-in-list second-song))
+(doseq [el first-song]
+  (println (type el)))
+
+(calculate-euclidean-distance-csv first-song second-song)
